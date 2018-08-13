6ee02874b8ce0ad3392d2b507c57b7d150581eef
Dilum Aluthge <dilum@aluthge.com>
2018-08-13 00:06:21 -0400

    Improve info messages in PredictMDExtra.__init__()

--- src/init.jl
+++ src/init.jl
@@ -2,7 +2,9 @@
 
 function __init__()::Nothing
     @info(string("This is PredictMDExtra, version ", version()))
+    @debug(string("PredictMDExtra debugging messages are enabled"))
     @info(string("For help, please visit https://www.predictmd.net"))
+    @info(string("PredictMD package directory: \"", PredictMD.pkg_dir(), "\""))
     return nothing
 end
 
