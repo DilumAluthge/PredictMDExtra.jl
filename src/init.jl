2f6de92340d173d7a9451c1982656fbdbf862891
Dilum Aluthge <dilum@aluthge.com>
2018-08-13 20:07:10 -0400

    Change info message to debug message

--- src/init.jl
+++ src/init.jl
@@ -2,9 +2,8 @@
 
 function __init__()::Nothing
     @info(string("This is PredictMDExtra, version ", version()))
-    @debug(string("PredictMDExtra debugging messages are enabled"))
     @info(string("For help, please visit https://www.predictmd.net"))
-    @info(string("PredictMDExtra package directory: \"", pkg_dir(), "\""))
+    @debug(string("PredictMDExtra package directory: \"", pkg_dir(), "\""))
     return nothing
 end
 
