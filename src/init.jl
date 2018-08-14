595479be0527764b1d8f31fc6da2accde8ec454d
Dilum Aluthge <dilum@aluthge.com>
2018-08-13 23:36:11 -0400

    Improve logging

--- src/init.jl
+++ src/init.jl
@@ -3,7 +3,7 @@
 function __init__()::Nothing
     println(string("This is PredictMDExtra, version ", version()))
     println(string("PredictMDExtra package directory: \"", pkg_dir(), "\""))
-    println(string("For help, please visit https://www.predictmd.net"))
+    @info(string("For help, please visit https://www.predictmd.net"))
     return nothing
 end
 
