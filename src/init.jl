6dd54a01410f0b68aa1f55c3d44faf0eaf8a4c62
Dilum Aluthge <dilum@aluthge.com>
2018-08-14 09:17:06 -0400

    Update init.jl

--- src/init.jl
+++ src/init.jl
@@ -1,7 +1,7 @@
 ##### Beginning of file
 
 function __init__()::Nothing
-    println(string("This is PredictMDExtra, version ", version()))
+    @info(string("This is PredictMDExtra, version ", version()))
     @debug(string("PredictMDExtra package directory: \"", pkg_dir(), "\""))
     @info(string("For help, please visit https://www.predictmd.net"))
     return nothing
