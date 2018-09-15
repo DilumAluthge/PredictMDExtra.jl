515c9989c0844a9af9f9c6f394864b7a7f24950b
Dilum Aluthge <dilum@aluthge.com>
2018-09-15 01:51:28 -0400

    Remove sneaky GPU stuff

--- deps/build.jl
+++ deps/build.jl
@@ -1,17 +1,3 @@
 ##### Beginning of file
 
-try
-    import Pkg
-    Pkg.add("CLArrays")
-    Pkg.build("CLArrays")
-catch
-end
-
-try
-    import Pkg
-    Pkg.add("CuArrays")
-    Pkg.build("CuArrays")
-catch
-end
-
 ##### End of file
