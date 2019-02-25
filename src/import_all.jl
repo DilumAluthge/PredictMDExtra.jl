628a2139f9cb8c6354e6814d2129e94a0520ea2c
Dilum Aluthge <dilum@aluthge.com>
2019-02-24 19:36:11 -0500

    Fix syntax

--- src/import_all.jl
+++ src/import_all.jl
@@ -10,10 +10,11 @@ function import_all(m::Module)
             m,
             Base.Meta.parse(
                 string(
-                    "try import $(string(s));",
-                    "@info(\"imported $(string(s))\");",
-                    "catch e1 @warn(\"ignoring exception: \", e1,);",
-                    "end",
+                    "try ",
+                    "import $(string(s)); ",
+                    "@info(\"imported $(string(s))\"); ",
+                    "catch e1 @warn(\"ignoring exception: \", e1,); ",
+                    "end ",
                     ),
                 ),
             )
@@ -23,17 +24,17 @@ function import_all(m::Module)
             m,
             Base.Meta.parse(
                 string(
-                    "try",
-                    "import $(string(p));",
-                    "catch e2",
-                    "@info(\"ignoring exception: \", e2,);",
-                    "try",
-                    "import Pkg;",
-                    "Pkg.add(\"$(string(p))\");",
-                    "catch e3",
-                    "@info(\"ignoring exception: \", e3,);",
-                    "end",
-                    "end",
+                    "try ",
+                    "import $(string(p)); ",
+                    "catch e2 ",
+                    "@info(\"ignoring exception: \", e2,); ",
+                    "try ",
+                    "import Pkg; ",
+                    "Pkg.add(\"$(string(p))\"); ",
+                    "catch e3 ",
+                    "@info(\"ignoring exception: \", e3,); ",
+                    "end ",
+                    "end ",
                     ),
                 ),
             )
@@ -41,12 +42,12 @@ function import_all(m::Module)
             m,
             Base.Meta.parse(
                 string(
-                    "try",
-                    "import $(string(p));",
-                    "@info(\"imported $(string(p))\");",
-                    "catch e4",
-                    "@warn(\"ignoring exception: \", e4,);",
-                    "end",
+                    "try ",
+                    "import $(string(p)); ",
+                    "@info(\"imported $(string(p))\"); ",
+                    "catch e4 ",
+                    "@warn(\"ignoring exception: \", e4,); ",
+                    "end ",
                     ),
                 ),
             )
